409a9cd0605f799ac92c9b0fba8da1e63ea20922
fdimorais <fdimorais@hotmail.com>
2019-05-16 19:59:16 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head><title>CRUD PHP</title>
+<meta charset="utf-8">
+</head>
+<?php require_once 'action/db_connect.php'; ?>
+<body>
+<h1>CRUD PHP</h1>
+<table border="1">
+<tr>
+<th>id</th> <!-- table header -->
+<th>Nome</th>
+<th>Razão Social</th>
+<th>Telefone</th>
+</tr>
+<tr>
+
+</tr>
+
+</table>
+
+
+</body>
+</html>
